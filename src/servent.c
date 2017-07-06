dffdedea118771f5420e869e47741ab81e15e7ee
Gleider Ribeiro Costa <gleiderr@gmail.com>
2017-07-06 20:53:48 -0300

    Mensagem CLIREQ (não compilado)

--- src/servent.c
+++ src/servent.c
@@ -10,39 +10,100 @@
 #include <errno.h>
 
 #define MAX_BUFF 512
+#define MAX_MSG 512
+#define TIMEOUT_SEC 4
+#define TIMEOUT_uSEC 0
+
+#define CLIREQ 1
+#define QUERY 2
 
 typedef struct {
-    char ip[16]; //000.000.000.000/0
-    int port;
+    struct in_addr sin_addr; //Utilizar inet_network() atribuindo o retorno para sin_addr.s_addr
+    uint16_t port;
 } Vizinho;
 
 typedef struct {
     int count;
-    Vizinhança vizinhanca[256];
+    Vizinhança vizinhos[256];
 } Vizinhanca;
+
+typedef struct {
+    uint16_t type;
+    uint16_t ttl;
+    struct in_addr sin_addr;
+    uint16_t port;
+    uint32_t seq;
+    char msg[MAX_MSG];
+} serventMsg;
 
 void die(char *s) {
     perror(s);
     exit(EXIT_FAILURE);
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
+    int i;
     int s; //Descritor do Socket 
     struct sockaddr_in sin;
+    socklen_t sin_len = sizeof(sockaddr_in);
+    Vizinhanca vizinhanca;
 
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
         die("error: socket");
     }
 
     int port = atoi(argv[1]); //Lendo e convertendo o Porto de Escuta
-    bzero((char*) sin, sizeof(struct sockaddr_in));
-    sin->sin_family = AF_INET;
-    sin->sin_addr.s_addr = htonl(INADDR_ANY);
-    sin->sin_port = htons(port);
-    if(bind(s, (struct sockaddr*) sin, sizeof(struct sockaddr_in)) < 0) {
+    bzero((char*) &sin, sizeof(struct sockaddr_in));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_port = htons(port);
+    if(bind(s, (struct sockaddr*) &sin, sin_len) < 0) {
         close(s);
         die("error: bind");
+    }
+
+    //Configurando timeout
+    struct timeval time_str;
+    time_str.tv_sec = TIMEOUT_SEC;
+    time_str.tv_usec = TIMEOUT_uSEC;
+    setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &time_str, sizeof(time_str));
+
+    //Leitura do arquivo
+    //Não implementada
+    /*char buff[MAX_BUFF];
+    while(!feof(stdin)) {
+        fgets(buff, MAX_BUFF, stdin);
+    }*/
+
+    serventMsg msg;
+    srand(port); //Porto como Semente, considerando que cada programa operará em um porto diferente
+    while(1) {
+        if (recv(s, &msg, sizeof(serventMsg), 0, &sin, &sin_len) < 0)
+            die("error: recvfrom");
+
+        switch(ntohs(msg.type)) {
+            case CLIREQ:
+                msg.type = htons(QUERY);
+                msg.ttl = htons(3);
+                msg.sin_addr = sin.sin_addr; //Não utilizo htons, pois à priori já chegou formatado
+                msg.port = sin.sin_port; //Não utilizo htons, pois à priori já chegou formatado
+                msg.seq = htonl(rand());
+                //msg.msg; não atribuido porque será enviada assim como recebida
+                for(i = 0; i < vizinhanca.count; i++) {
+                    sin.sin_addr = vizinhanca.vizinhos[i].sin_addr;
+                    sin.sin_port = vizinhanca.vizinhos[i].port;
+                    if (sendto(s, &msg, sizeof(serventMsg), 0, (struct sockaddr*) &sin, slen) < 0)
+                        die("error: sendto");
+                }
+
+                //procurar a chave no dicionário local e responder ao cliente se a chave encontrada
+
+                break;
+            case QUERY:
+                break;
+            default:
+                die("error: switch");
+        }
     }
 
     exit(EXIT_SUCCESS);
