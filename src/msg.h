93142792bb772c4b431b40532999957e1cc8b2c2
Gleider Ribeiro Costa <gleiderr@gmail.com>
2017-07-07 20:11:30 -0300

    Modularizando e melhorias de código

---
+++ src/msg.h
@@ -0,0 +1,38 @@
+#ifndef MSG_H
+#define MSG_H
+
+#define CLIREQ 1
+#define QUERY 2
+#define RESPONSE 3
+
+#define MAX_CHAVE 41
+#define MAX_VALOR 161
+
+typedef struct {
+    uint16_t type; //Sempre igual a QUERY
+    uint16_t ttl;
+    struct in_addr sin_addr;
+    uint16_t port;
+    uint32_t seq;
+    char chave[MAX_CHAVE];
+} Msg_query;
+
+typedef struct {
+    uint16_t type; //Sempre igual a CLIREQ
+    char chave[MAX_CHAVE];
+} Msg_clireq;
+
+typedef struct {
+    uint16_t type;
+    char chave_valor[MAX_CHAVE + MAX_VALOR];
+} Msg_response;
+
+#define Msg_generica Msg_query
+
+/* Verifica se determinada Msg_query está na memória */
+int inQueryMemory(Msg_query* msg);
+
+/* Insere determinada query na memória */
+void insertQueryMemory(&msg);
+
+#endif
---
+++ src/neighborhood.h
@@ -0,0 +1,21 @@
+#ifndef VIZINHANCA_H
+#define VIZINHANCA_H
+
+#include "msg.h"
+
+typedef struct {
+    struct in_addr sin_addr; //Utilizar inet_network() atribuindo o retorno para sin_addr.s_addr
+    uint16_t port;
+} Neighbor;
+
+typedef struct {
+    int count;
+    Neighbor neighbors[256];
+} Neighborhood;
+
+/* Constrói vizinhança a partir da lista de endereços [argv], iniciando no índice [begin], cujo [argv] contém [n] elementos. */
+void buildNeighborhood(char const *argv[], int begin, int n, Neighborhood* neighborhood);
+
+dispatch(Msg_query* msg_query, Neighborhood* neighborhood, struct sockaddr_in* sin);
+
+#endif
